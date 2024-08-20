72548f90d00fd6cf8aa636c4130fee3580049de5
Daniele-Cassia <danielecassia9@gmail.com>
2024-08-20 09:50:40 -0300

    define TODOs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+#define all(X) begin(X), end(X)
+#define ll long long int
+
+typedef vector<hash<ll>> Sections;
+typedef vector<hash<ll>> Tricks;
+
 int main() {
-    cout << "Hello World!"<<endl;
+    int N, K;
+    cin>>N>>K;
+
+    Sections s(N);
+    Tricks t(K);
+
+    // ler quantidade de sessões e manobra (N e K);
+    // cria estrura para guardar os dados
+    // guarda os dados
+    
+    // PARA CADA SESSAO: 
+        // chama dp e retorna a soma da de bonificação da melhor sequencia
+        //calcula e imprime pontuação final (retorno de dp * bonificação da sessao*qtd de manobra usadas)
+
     return 0;
 }
